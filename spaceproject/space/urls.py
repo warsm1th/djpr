ec4f7ea5ea4141c0c38125b869c0e3c49a070cc4
warsm1th <symba9x@gmail.com>
2023-04-05 21:59:21 +0300

    first

--- spaceproject/space/urls.py
+++ spaceproject/space/urls.py
@@ -4,6 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('categories/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>\d{4})/', archive)
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('about/', about, name='about'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category')
 ]
--- spaceproject/space/views.py
+++ spaceproject/space/views.py
@@ -1,20 +1,64 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import *
 
-menu =['О сайте', "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}]
+
 
 def index(request):
-    return render(request, 'space/index.html', {'menu': menu, 'title': 'Главная страница'})
+    posts = Objects.objects.all()
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'cats': cats,
+        'title': "Главная страница",
+        'cat_selected': 0,
+    }
+    return render(request, 'space/index.html', context=context)
 
 
-def categories(request, catid):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+def about(request):
+    context = {
+        'menu': menu,
+        'title': "О сайте"
+    }
+    return render(request, 'space/about.html', context=context)
 
 
-def archive(request, year):
-    if int(year) > 2020 or int(year) < 2000:
-        return redirect('home', permanent=True)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def addpage(request):
+    return HttpResponse('Добавить статью')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = Objects.objects.filter(cat_id=cat_id)
+    if len(posts) == 0:
+        raise Http404()
+    
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'cats': cats,
+        'title': "Отображение по рубрикам",
+        'cat_selected': cat_id,
+    }
+    return render(request, 'space/index.html', context=context)
 
 
 def pageNotFound(request, exception):
