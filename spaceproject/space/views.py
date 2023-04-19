28d6694174ff95e733536d26c673a32bd07b7297
warsm1th <symba9x@gmail.com>
2023-04-19 15:34:22 +0300

    19.04.23 15.35

--- spaceproject/space/views.py
+++ spaceproject/space/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import *
 from .forms import *
 
 
-def index(request):
-    posts = Objects.objects.all()
-    context = {
-        'posts': posts,
-        'title': "Главная страница",
-        'cat_selected': 0,
-    }
-    return render(request, 'space/index.html', context=context)
+class SpaceHome(ListView):
+    model = Objects
+    template_name = 'space/index.html'
+    context_object_name = 'posts'
+
+    # extra_context = {'title': 'Главная страница'}   # Добавляем статический контекст
+
+    # Функция для формирования динамического и статического контекста
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)  # Сохраняем сформированный контекст
+        # context['menu'] = menu    # Добавляем динамический контекст
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+
+        return context
+
+    def get_queryset(self):  # Настройка запроса из БД
+        return Objects.objects.filter(is_published=True)  # Фильтруем записи из БД
 
 
 def about(request):
@@ -40,30 +51,38 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Objects, slug=post_slug)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Objects, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'space/post.html', context=context)
 
-    context = {
-        'post': post,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
 
-    return render(request, 'space/post.html', context=context)
+class ShowPost(DetailView):
+    model = Objects
+    template_name = 'space/post.html'
 
 
-def show_category(request, cat_slug):
-    cat = Category.objects.get(slug=cat_slug)
-    posts = Objects.objects.filter(cat_id=cat.pk)
-    if len(posts) == 0:
-        raise Http404()
+class SpaceCategory(ListView):
+    model = Objects
+    template_name = 'space/index.html'
+    context_object_name = 'posts'
+    allow_empty = False     # При отсутствии записей формируется ошибка 404
 
-    context = {
-        'posts': posts,
-        'title': "Отображение по рубрикам",
-        'cat_selected': cat.pk,
-    }
-    return render(request, 'space/index.html', context=context)
+    def get_queryset(self):
+        return Objects.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+
+        return context
 
 
 def pageNotFound(request, exception):
